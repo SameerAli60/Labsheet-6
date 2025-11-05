69de0a6a72bd00486d3187478b1e20a15755026e
SameerAli60 <cu25260290@coeruniversity.ac.in>
2025-11-05 12:59:26 +0530

    Create 19.cpp
    Implement insertion sort for a character vector.

---
+++ 19.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ int n; cin>>n; vector<char> a(n);
+ for(int i=0;i<n;i++) cin>>a[i];
+ for(int i=1;i<n;i++){
+ char key = a[i]; int j = i-1;
+ while(j >= 0 && a[j] > key){ a[j+1] = a[j]; j--; }
+ a[j+1] = key;
+ }
+ for(char c: a) cout<<c<<' ';
+ cout<<"\
