cda5ce7fa7cd62702684e9644ce9ec4e4cfc36b1
SameerAli60 <cu25260290@coeruniversity.ac.in>
2025-11-05 12:49:10 +0530

    Create 13.cpp
    Implement selection sort algorithm to sort an array.

---
+++ 13.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ int n; cin>>n; vector<int> a(n);
+ for(int i=0;i<n;i++) cin>>a[i];
+ for(int i=0;i<n-1;i++){
+ int minidx = i;
+ for(int j=i+1;j<n;j++) if(a[j] < a[minidx]) minidx = j;
+ swap(a[i], a[minidx]);
+ }
+ for(int x: a) cout<<x<<' ';
+ cout<<"\n";
+ return 0;
+}
